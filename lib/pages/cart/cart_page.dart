6d2aad8922a0a0ddffe2a5b724d1690300001a09
Valen754 <hllzrd@gmail.com>
2025-12-08 19:19:59 +0700

    app_widgets

--- lib/pages/cart/cart_page.dart
+++ lib/pages/cart/cart_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import '../../../backup/models/data.dart';
-import '../../../backup/pages/cart/checkout_page.dart';
+import '../../../models/data.dart';
+import '../cart/checkout_page.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
---
+++ lib/widgets/app_widgets.dart
@@ -0,0 +1,6 @@
+// Barrel file: re-export modular widgets for easier maintenance
+
+export '../../widgets/navigation/nav_bar_item.dart';
+export '../../widgets/product/product_card.dart';
+export '../../widgets/product/menu_product_card.dart';
+export '../../widgets/misc/placeholder_screen.dart';
