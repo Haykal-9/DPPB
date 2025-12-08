a9171623a86e6264c35cc8885ffcc22e9403b8bc
salmanridhwan <salmanridhwan04@gmail.com>
2025-12-08 15:25:56 +0700

    model untuk produk

---
+++ lib/models/data.dart
@@ -0,0 +1,11 @@
+// Models
+export 'product.dart';
+export 'cart_item.dart';
+export 'comment.dart';
+export 'order_status.dart';
+
+export '../data/product_data.dart';
+export '../data/cart_data.dart';
+export '../data/comment_data.dart';
+export '../data/order_data.dart';
+export '../data/category_data.dart';
