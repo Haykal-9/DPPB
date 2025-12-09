214df516e4426ff4abbda6184d4bfd22c5587a1c
Valen754 <hllzrd@gmail.com>
2025-12-09 07:47:31 +0700

    foto di cart sudah diperbaiki

--- lib/data/cart_data.dart
+++ lib/data/cart_data.dart
@@ -3,10 +3,17 @@ import '../models/cart_item.dart';
 
 /// Cart Items
 final List<CartItem> mockCartItems = [
-  CartItem(const Product('Classic Caffe Latte', 5.50, 'latte.jpg'), 1),
+  CartItem(const Product('Latte', 25000.0, 'assets/images/kopi/Latte.jpg'), 1),
   CartItem(
-    const Product('Iced Caramel Macchiato', 6.25, 'iced_caramel.jpg'),
+    const Product('Cappucino', 22000.0, 'assets/images/kopi/CAPPUCINO.jpg'),
     2,
   ),
-  CartItem(const Product('Avocado & Egg Sandwich', 7.00, 'sandwich.jpg'), 1),
+  CartItem(
+    const Product(
+      'ES Kopi Susu',
+      22000.0,
+      'assets/images/kopi/ES KOPI SUSU.jpg',
+    ),
+    1,
+  ),
 ];
