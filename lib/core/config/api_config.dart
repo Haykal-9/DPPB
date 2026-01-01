12459fcb3bcd7e04c1ad6048e951f81d0b373b68
Vemasseftaesadwisetiawan <vmasstwn@gmail.com>
2026-01-01 16:06:04 +0700

    .

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -2,5 +2,5 @@ class ApiConfig {
   // Ganti dengan IP laptop kamu jika pakai HP fisik
   // kalau pake emulator ganti dengan 10.0.2.2
   // punya salman : http://192.168.1.14:8000/api/
-  static const String baseUrl = "http://192.168.1.14:8000/api/";
+  static const String baseUrl = "http://192.168.1.5:8000/api/";
 }
